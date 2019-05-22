c72e494185ae288ba723fd621843f59ae1df9839
Jovan <abc.j.jovan@gmail.com>
2019-05-22 08:29:51 +0200

    Initial commit, working sensor

---
+++ Arduino_Photosensor.ino
@@ -0,0 +1,15 @@
+int sensorPin = A0; // LDR pin
+int led = 9; 
+
+int sensorValue = 0; // Primanje vrednosti senzora
+void setup() {
+Serial.begin(9600);
+pinMode(led, OUTPUT);
+}
+void loop() {
+sensorValue = analogRead(sensorPin); // Citanje vrednosti senzora
+Serial.println(sensorValue); 
+analogWrite(led, sensorValue); //Uspesno kontrolise senzor
+delay(100);
+
+}
