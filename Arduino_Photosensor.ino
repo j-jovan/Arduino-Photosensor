49e9b8bc1aabd133efa86625ab872782d1b67919
Jovan <abc.j.jovan@gmail.com>
2019-05-22 08:37:04 +0200

    Add rough photosensor controls

--- Arduino_Photosensor.ino
+++ Arduino_Photosensor.ino
@@ -9,7 +9,14 @@ pinMode(led, OUTPUT);
 void loop() {
 sensorValue = analogRead(sensorPin); // Citanje vrednosti senzora
 Serial.println(sensorValue); 
-analogWrite(led, sensorValue); //Uspesno kontrolise senzor
+//analogWrite(led, sensorValue); //Uspesno kontrolise senzor
+
+if(sensorValue < 300 and sensorValue>100){
+  digitalWrite(led, HIGH);
+}
+if(sensorValue>300){
+  digitalWrite(led, LOW);
+}
 delay(100);
 
 }
